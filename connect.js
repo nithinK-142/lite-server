4813b85ccea982182b7168587d34beae686090ce
nithinK-142 <nithin.sagar.359@gmail.com>
2024-06-26 21:01:07 +0530

    update: sqlite db setup.

---
+++ connect.js
@@ -0,0 +1,33 @@
+import sqlite3 from "sqlite3";
+
+const sql3 = sqlite3.verbose();
+
+const liteDB = new sql3.Database(
+  "./litedb.db",
+  sqlite3.OPEN_READWRITE,
+  connected
+);
+
+function connected(err) {
+  if (err) {
+    console.log("error connecting to db", err.message);
+    return;
+  }
+  console.log("Created the DB or it already exists!");
+}
+
+const createDB = `CREATE TABLE IF NOT EXISTS animelist (
+    anime_id INTEGER PRIMARY KEY,
+    anime_name TEXT NOT NULL,
+    anime_description TEXT NOT NULL,
+    isfavorite BOOLEAN NOT NULL DEFAULT FALSE)`;
+
+liteDB.run(createDB, [], (err) => {
+  if (err) {
+    console.log("error creating animelist table ", err.message);
+    return;
+  }
+  console.log("Table created.");
+});
+
+export { liteDB };
